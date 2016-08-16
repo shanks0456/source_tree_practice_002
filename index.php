230d74c59c1da5141335a9987d588396a2f38768
鈴木健人 <13bn075c@rikkyo.ac.jp>
2016-08-16 14:18:55 +0800

    add robot class

--- index.php
+++ index.php
@@ -8,6 +8,19 @@
 
 	$result = add(1,1);
 	echo $result;
-	
+
+	class Robot {
+		private $name;
+
+		function setName($name) {
+			$this->name = $name;
+		}
+		function getName() {
+			return $this->name;
+		}
+	}
+
+	$robot = new Robot();
+	$robot->setName('ドラえもん');
  ?>
  Initial commit
