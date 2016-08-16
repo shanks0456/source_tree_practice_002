c808a3ef5791bf477a8b5d80525f1efaae943cd5
鈴木健人 <13bn075c@rikkyo.ac.jp>
2016-08-16 15:13:14 +0800

    create org_echo function for debug env.

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php 
+	define('DEBUG', TRUE);
+
 	echo "Heloo world";
 
 	function add($num1, $num2) {
@@ -33,5 +35,13 @@
 	echo $result;
 	
 >>>>>>> minus_function
+
+	// デバッグ用関数の定義
+	function org_echo($val) {
+		if (DEBUG) {
+			echo $val;
+			echo '<br>';
+		}
+	}
  ?>
  Initial commit
